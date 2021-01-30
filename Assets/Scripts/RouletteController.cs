4ddda8a2213b21d4c0c8f4e3c5c1ff521c71e461
mitsuru_yamaura <komayou326@gmail.com>
2021-01-30 23:33:16 +0900

    初回コミット。課題作成用。作成済。

---
+++ Assets/Scripts/RouletteController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0;
+
+    public float velocity = 5.0f;
+
+    bool isReverse = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) {
+            this.rotSpeed = 0;
+            Debug.Log("Stop");
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            //isReverse = !isReverse;
+
+            if (isReverse == false) {
+                isReverse = true;
+                Debug.Log("Reverse");
+            } else {
+                isReverse = false;
+                Debug.Log("Normal");
+            }
+        }
+
+
+
+        if (Input.GetMouseButtonDown(0)) {
+
+            //this.rotSpeed = isReverse ? -velocity : velocity;
+
+
+            if (isReverse == false) {
+                this.rotSpeed = velocity;
+            } else {
+                this.rotSpeed = -velocity;
+            }
+
+
+            //this.rotSpeed = -10f;
+        }
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        //this.rotSpeed *= 0.96f;
+    }
+}
