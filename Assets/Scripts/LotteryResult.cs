ce8ee106b341ad4b90b5944a386012baacf07e5c
mitsuru_yamaura <komayou326@gmail.com>
2021-04-18 12:27:21 +0900

    コード修正。コメント追加。

--- Assets/Scripts/LotteryResult.cs
+++ Assets/Scripts/LotteryResult.cs
@@ -2,18 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+/// <summary>
+/// 一定時間ごとに抽選して中世結果を画面表示
+/// </summary>
 public class LotteryResult : MonoBehaviour
 {
-    public enum LottelyType {
-        大吉,
-        中吉,
-        小吉,
-        末吉,
-        凶,
-        大凶
-    }
-
     private GameObject lotteryResultObj;
     private bool isLottery;
 
@@ -35,6 +30,7 @@ public class LotteryResult : MonoBehaviour
             timer += Time.deltaTime;
 
             if (timer > lotteryTime) {
+                // おみくじの抽選
                 SubmitLottery();
             }
         }
@@ -44,51 +40,27 @@ public class LotteryResult : MonoBehaviour
     /// おみくじの抽選
     /// </summary>
     private void SubmitLottery() {
-        //if (transform.rotation.z >= 30 && transform.rotation.z < 90) {
-        //    lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "大吉";
-        //    //lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = LottelyType.大吉.ToString();
-        //} else if (transform.rotation.z >= 90 && transform.rotation.z < 150) {
-        //    lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "大凶";
-        //} else if (transform.rotation.z >= 150 && transform.rotation.z > -150) {
-        //    lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = LottelyType.小吉.ToString();
-        //} else if (transform.rotation.z <= -150 && transform.rotation.z > -90) {
-        //    lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = LottelyType.末吉.ToString();
-        //} else if (transform.rotation.z <= -90 && transform.rotation.z > -30) {
-        //    lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "中吉";
-        //} else if (transform.rotation.z <= -30 && transform.rotation.z < 30){
-        //    DisplayMessage(LottelyType.凶.ToString());
-        //}
 
-        // 再抽選のための準備
-        //transform.rotation = new Quaternion(0, 0, 0, 0);
-
-        if (transform.rotation.z > -0.166f && transform.rotation.z < 0.166f) {
+        // eulerAngles を利用すると、0 ～ 360 の角度が取得できる。rotation を利用すると 0 ～ 1.0f, 1.0f ～ -1.0f になるので注意
+        if (transform.eulerAngles.z >= 30 && transform.eulerAngles.z < 90) {
             lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "大吉";
-
-        } else if (transform.rotation.z >= 0.166f && transform.rotation.z < 0.5f) {
+        } else if (transform.eulerAngles.z >= 90 && transform.eulerAngles.z < 150) {
             lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "大凶";
-        } else if (transform.rotation.z >= 0.5f && transform.rotation.z < 0.83f ) {
-            lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = LottelyType.小吉.ToString();
-        } else if (transform.rotation.z >= 0.83f && transform.rotation.z > -0.83f) {
-            lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = LottelyType.末吉.ToString();
-        //} else if () {
-        //    lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "中吉";
-        //} else if () {
-        //    DisplayMessage(LottelyType.凶.ToString());
+        } else if (transform.eulerAngles.z >= 150 && transform.eulerAngles.z < 210) {
+            lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "小吉";
+        } else if (transform.eulerAngles.z >= 210 && transform.eulerAngles.z < 270) {
+            lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "末吉";
+        } else if (transform.eulerAngles.z >= 270 && transform.eulerAngles.z < 330) {
+            lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "中吉";
+        } else {
+            lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "凶";
         }
 
+        // 再抽選のための準備
         timer = 0;
 
         isLottery = false;
 
-        Debug.Log(transform.rotation.z);
-    }
-
-    /// <summary>
-    /// おみくじ結果の画面表示更新
-    /// </summary>
-    /// <param name="message"></param>
-    private void DisplayMessage(string message) {
-        lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = message;
+        Debug.Log(transform.eulerAngles.z);
     }
 }
--- Assets/Scripts/NeedleController.cs
+++ Assets/Scripts/NeedleController.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class NeedleController : MonoBehaviour
 {
-    private GameObject lotteryResultObj;
     private UnityEngine.UI.Text txtlotteryResult;
 
     void Start() {
-        lotteryResultObj = GameObject.Find("LotteryResult");
-        txtlotteryResult = lotteryResultObj.GetComponent<UnityEngine.UI.Text>();
+        txtlotteryResult = GameObject.Find("LotteryResult").GetComponent<UnityEngine.UI.Text>();
     }
 
     private void OnTriggerStay2D(Collider2D collision) {
@@ -18,7 +16,8 @@ public class NeedleController : MonoBehaviour
         Lottery lottery;
 
         if (collision.TryGetComponent(out lottery)) {
-            txtlotteryResult.text = lottery.lotteryType.ToString();
+            // おみくじ結果の画面表示更新
+            DisplayLotteryResultMessage(lottery.lotteryType.ToString());
             return;
         }
 
@@ -26,22 +25,22 @@ public class NeedleController : MonoBehaviour
         if (collision.TryGetComponent(out lottery)) {
             switch (lottery.lotteryType) {
                 case Lottery.LotteryType.大吉:
-                    lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "大吉";
+                    txtlotteryResult.text = "大吉";
                     break;
                 case Lottery.LotteryType.中吉:
-                    lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "中吉";
+                    txtlotteryResult.text = "中吉";
                     break;
                 case Lottery.LotteryType.小吉:
-                    lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "小吉";
+                    txtlotteryResult.text = "小吉";
                     break;
                 case Lottery.LotteryType.末吉:
-                    lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "末吉";
+                    txtlotteryResult.text = "末吉";
                     break;
                 case Lottery.LotteryType.凶:
-                    lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "凶";
+                    txtlotteryResult.text = "凶";
                     break;
                 case Lottery.LotteryType.大凶:
-                    lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "大凶";
+                    txtlotteryResult.text = "大凶";
                     break;
             }
             return;
@@ -49,17 +48,25 @@ public class NeedleController : MonoBehaviour
 
 
         if (collision.gameObject.name == "daikichi") {
-            lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "大吉";
+            txtlotteryResult.text = "大吉";
         } else if (collision.gameObject.name == "chukichi") {
-            lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "中吉";
+            txtlotteryResult.text = "中吉";
         } else if (collision.gameObject.name == "shoukichi") {
-            lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "小吉";
+            txtlotteryResult.text = "小吉";
         } else if (collision.gameObject.name == "suekichi") {
-            lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "末吉";
+            txtlotteryResult.text = "末吉";
         } else if (collision.gameObject.name == "kyo") {
-            lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "凶";
+            txtlotteryResult.text = "凶";
         } else if (collision.gameObject.name == "daikyo") {
-            lotteryResultObj.GetComponent<UnityEngine.UI.Text>().text = "大凶";
+            txtlotteryResult.text = "大凶";
         }
+    }
+
+    /// <summary>
+    /// おみくじ結果の画面表示更新
+    /// </summary>
+    /// <param name="message"></param>
+    private void DisplayLotteryResultMessage(string message) {
+        txtlotteryResult.text = message;
     }
 }
